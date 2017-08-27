aa9d7598b0d6e9fb36bd73fcba8f52c31ec62dda
方云鹤 <842241090@qq.com>
2017-08-28 00:14:05 +0800

    druid 配置好了

--- src/main/java/org/fyh/spirngdemo2/App.java
+++ src/main/java/org/fyh/spirngdemo2/App.java
@@ -5,12 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@ServletComponentScan
 @MapperScan("org.fyh.spirngdemo2.mapper")
 public class App {
 	private static final Logger logger = LoggerFactory.getLogger(App.class);
