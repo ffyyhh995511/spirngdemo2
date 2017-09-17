f79ec0d102b4380e84fb1346f734e800a100080d
方云鹤 <842241090@qq.com>
2017-09-17 11:29:59 +0800

    7

--- src/main/java/org/fyh/spirngdemo2/configuration/DruidDBConfig.java
+++ src/main/java/org/fyh/spirngdemo2/configuration/DruidDBConfig.java
@@ -4,13 +4,13 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import com.alibaba.druid.pool.DruidDataSource;
 
 @Configuration
